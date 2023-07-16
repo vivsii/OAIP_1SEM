779c579bd8412fa65a360ff04842f0b9881ede01
vivsii <116980278+vivsii@users.noreply.github.com>
2023-07-16 22:58:20 +0300

    Create format.cpp

---
+++ laba4/format.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	printf("\n\t\t\t Евсеенко\n");
+	printf("\n\t\t\t\b Виктория\n");
+	printf("\n\t\t Павловна\n");
+	printf("\n\tФакультет Информационных технологий");
+	printf("\n\tГруппа 4\n");
+	_getch();
+}
