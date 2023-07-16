d3fd7f9144abfca0706316688a8403269c129c5f
vivsii <116980278+vivsii@users.noreply.github.com>
2023-07-17 00:04:35 +0300

    Create var04_2.cpp

---
+++ laba11/var04_2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_CTYPE, "Russian");
+    int A, p, n, razn;
+    char s[33];
+    cout << "Введите число" << endl;
+    cin >> A;
+    _itoa_s(A, s, 2);
+    cout << s << endl;
+    cout << "Введите начальную позицию" << endl;
+    cin >> p;
+    cout << "Введите число битов" << endl;
+    cin >> n;
+    razn = p - n;
+    while (p >= razn)
+    {
+        A |= 1 << p;
+        _itoa_s(A, s, 2);
+        p--;
+    }
+    cout << s << endl;
+    return 0;
+}
