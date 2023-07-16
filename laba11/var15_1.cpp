4c3995b6c5ec2073bf629664d6201bed6fddc831
vivsii <116980278+vivsii@users.noreply.github.com>
2023-07-17 00:02:21 +0300

    Create var15_1.cpp

---
+++ laba11/var15_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std;
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	int A; char s[33];
+	cout << "Введите число ";
+	cin >> A;
+	_itoa_s(A, s, 2);
+	cout << "Число в двоичном виде = " << s << endl;
+	if ((A & 1) == 0)
+		cout << "Число кратно 2" << endl;
+	else
+		cout << "Число не кратно 2" << endl;
+	return 0;
+}
